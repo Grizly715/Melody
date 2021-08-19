c859881caef4ebfc4f4baf84f899d91c56d35dff
Grizly <burichin7@gmail.com>
2021-08-19 18:49:02 +0600

    lesson-3

---
+++ js/main.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+	var currentFloor = 2; // переменная с текущем этажем 
+	var floorPath = $(".home-image path"); /* Номер этажа */
+	var counterUp = $(".counter-up"); /* Кнопка увеличения этажа */
+	var counterDown = $(".counter-down"); /* Кнопка уменьшания этажа */
+
+	// Функция подсветки этажа при наведение мышки 
+	floorPath.on('mouseover', function () {
+		floorPath.removeClass("current-floor");
+		currentFloor = $(this).attr("data-floor"); // получаем значение текущего этажа 
+		$(".counter").text(currentFloor); // Записываем значение этажа в счетчик справа
+	});
+
+	counterUp.on("click", function () { // остеживаем клик по кнопке вверх
+		if (currentFloor < 18) {
+			currentFloor++;
+			usCurrentFloor = currentFloor.toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false }); // Форматируем счетчик после увеличения на 1, вместо 2 получем 02
+			$(".counter").text(usCurrentFloor);
+			floorPath.removeClass("current-floor");
+			$(`[data-floor =${usCurrentFloor}]`).toggleClass("current-floor"); // подсвечиваем нужный этаж
+		}
+	});
+
+	counterDown.on("click", function () { // остеживаем клик по кнопке вниз
+		if (currentFloor > 2) {
+			currentFloor--;
+			usCurrentFloor = currentFloor.toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false });
+			$(".counter").text(usCurrentFloor);
+			floorPath.removeClass("current-floor");
+			$(`[data-floor =${usCurrentFloor}]`).toggleClass("current-floor");
+		}
+	});
+});
